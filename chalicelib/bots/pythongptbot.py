462b8e97b706746f6f946349f32ecb4ad9385619
Aymeric <133921099+DevRickyCst@users.noreply.github.com>
2024-01-09 01:40:50 +0100

    Update pythongptbot.py

--- chalicelib/bots/pythongptbot.py
+++ chalicelib/bots/pythongptbot.py
@@ -2,7 +2,7 @@ from chalicelib.src.gpt import Gpt
 from chalicelib.src.message import Message
 from chalicelib.src.telegram import TelegramInterface
 
-gpt = Gpt(api_key="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")
+gpt = Gpt(api_key=os.environ["gpt_key"])
 
 
 def handle_message(msg: Message, telegram: TelegramInterface):
